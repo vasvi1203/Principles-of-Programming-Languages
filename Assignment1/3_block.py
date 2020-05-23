38ad3d5627aa46d2238431333cff1a57854e1a1f
vasvi1203 <64128681+vasvi1203@users.noreply.github.com>
2020-05-23 14:02:11 +0530

    Add files via upload

--- Assignment1/3_block.py
+++ Assignment1/3_block.py
@@ -3,39 +3,36 @@
 import time
 from datetime import datetime as dt
 
-hosts_path = "/etc/hosts"
+hosts_path = "C:\Windows\System32\drivers\etc\hosts"		# /etc/hosts for linux
 
 redirect = "127.0.0.1"
 
 sites = int(input('How many sites you want to block?\n'))
 
-for i in range(1, sites + 1) :
-	lst = []
+lst = []
+for i in range(0, sites):
 	website = input('Enter the site url to block : ')
 	lst.append(website)
-	
+
 time_int = input('Enter the time interval during which you want to block the site separated by a space : ').split()
 
-start = time_int[0]
-end = time_int[1]
+start = int(time_int[0])
+end = int(time_int[1])
 
-while True:
-	if dt(dt.now().year, dt.now().month, dt.now().day, start) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, end) :
-		with open(hosts_path, 'r+') as file :
-			content = file.read()
-			for web in lst :
-				if web in content:
-					pass
-				else :
-					file.write(redirect + " " + web + "\n")					
-	else : 
-		with open(hosts_path, 'r+') as file:
-			content = file.readlines()
-			file.seek(0)
-			for line in content :
-				if not any(web in line for web in lst) :
-					file.write(line)
-			file.truncate()			
-	time.sleep(5) 
-	
-	
+if dt(dt.now().year, dt.now().month, dt.now().day, start) < dt.now() < dt(dt.now().year, dt.now().month, dt.now().day, end):
+	with open(hosts_path, 'r+') as file:
+		content = file.read()
+		for web in lst:
+			if web in content:
+				pass
+			else:
+				file.write(redirect + "  " + web + "\n")		
+			print("Blocked " + web)			
+else: 
+	with open(hosts_path, 'r+') as file:
+		content = file.readlines()
+		file.seek(0)
+		for line in content:
+			if not any(web in line for web in lst):
+				file.write(line)
+		file.truncate()	
