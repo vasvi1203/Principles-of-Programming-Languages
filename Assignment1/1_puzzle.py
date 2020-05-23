14efd8f5d6ebb72f10341200b031798be6d4dc80
vasvi1203 <64128681+vasvi1203@users.noreply.github.com>
2020-05-23 18:31:29 +0530

    Update 1_puzzle.py

--- Assignment1/1_puzzle.py
+++ Assignment1/1_puzzle.py
@@ -1,7 +1,5 @@
 '''Solve the puzzle with a Python program: A man is walking down the village road with a tiger, a goat and a bundle of grass. Soon he arrives at the river bank where there is one tiny boat that can carry him and another animal or grass at a time. Here is the problem : Left alone, the tiger will eat the goat. And similarly, the goat will eat the grass bundle. How is he going to take all three across the river safely?'''
 
-'''Solve the puzzle with a Python program: A man is walking down the village road with a tiger, a goat and a bundle of grass. Soon he arrives at the river bank where there is one tiny boat that can carry him and another animal or grass at a time. Here is the problem : Left alone, the tiger will eat the goat. And similarly, the goat will eat the grass bundle. How is he going to take all three across the river safely?'''
-
 b1 = {"grass", "goat", "tiger"}
 b2 = set()
 case1 = {"grass", "goat"}
